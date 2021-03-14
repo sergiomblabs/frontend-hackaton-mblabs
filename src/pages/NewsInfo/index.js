2268789d1cae615ec78360601f0d33497701901d
Lohan Martz <lohan.fidelis@gmail.com>
2021-03-14 11:39:14 -0300

    Info News Scroll

--- src/pages/NewsInfo/index.js
+++ src/pages/NewsInfo/index.js
@@ -27,6 +27,8 @@ const useStyles = makeStyles((theme) => ({
     marginTop: '30px',
     paddingLeft: '10px',
     paddingRight: '10px',
+    position: 'justify',
+    overflow: 'auto'
   },
   newsList: {
     marginTop: theme.spacing(2),
@@ -88,7 +90,7 @@ export default function NewsInfo() {
         <Box className={classes.cardStyle}>
           <Typography align="justify" className="complete-text">
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem.
-          In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy. Fusce aliquet pede non pede. Suspendisse dapibus lorem pellentesque magna.
+          In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy. Fusce aliquet pede non pede. Suspendisse dapibus lorem pellentesque magna. Donec blandit feugiat ligula. Donec hendrerit, felis et imperdiet euismod, purus ipsum pretium metus, in lacinia nulla nisl eget sapien.
         </Typography>
         </Box>
       </Box>
